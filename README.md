2c5f937cf423fd1686e6b81c2303e679be751489
Peter Berry <pwcberry@gmail.com>
2025-09-17 20:31:54 +1000

    Add test capability

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Travels In Code Experiment: Spotify API
 
+Let's learn how to use the Web API provided by Spotify.
+
 ## Getting Started
 
--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
+import mochaPlugin from "eslint-plugin-mocha";
 
 export default defineConfig([
   globalIgnores(["node_modules"]),
@@ -14,6 +15,12 @@ export default defineConfig([
         ecmaVersion: "latest",
         sourceType: "module",
       },
+    },
+  },
+  mochaPlugin.configs.flat.recommended,
+  {
+    rules: {
+      "mocha/no-mocha-arrows": "off",
     },
   },
 ]);
