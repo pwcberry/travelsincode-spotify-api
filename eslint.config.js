0c1cae4ac5f6faa0efff5cae5737c93aaa56c9fd
Peter Berry <pwcberry@gmail.com>
2025-11-18 21:58:57 +1100

    Lint browser JavaScript

--- eslint.config.js
+++ eslint.config.js
@@ -6,13 +6,27 @@ import mochaPlugin from "eslint-plugin-mocha";
 export default defineConfig([
   globalIgnores(["node_modules", ".pnp.cjs", ".pnp.loader.mjs"]),
   {
-    files: ["**/*.js"],
+    files: ["lib/*.js", "test/**/*.js"],
     extends: [js.configs.recommended],
     languageOptions: {
       ecmaVersion: 2021,
       globals: {
         ...globals.node,
         td: "readonly",
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+  },
+  {
+    files: ["public/assets/*.js"],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2023,
+      globals: {
+        ...globals.browser,
       },
       parserOptions: {
         ecmaVersion: "latest",
